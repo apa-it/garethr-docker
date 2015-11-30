86911ffe93c843efe2af2dde6e4ee8038a1a3699
Christian Tawfik <christian.tawfik@apa.at>
2015-11-30 20:07:22 +0100

    fixed typo in hash definition

--- manifests/run_instance.pp
+++ manifests/run_instance.pp
@@ -1,6 +1,6 @@
 # docker::run_instance
 class docker::run_instance {
-   $instances = hiera_hash('docker::run_instances', undef)
+   $instances = hiera_hash('docker::run_instance', undef)
    if ($instances) {
       validate_hash($instances)
       create_resources(docker::run, $instances)
