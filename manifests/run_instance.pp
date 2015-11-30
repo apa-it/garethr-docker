3393e4b14d5723058020b229b2cfe615e687e4d6
Christian Tawfik <christian.tawfik@apa.at>
2015-11-30 18:10:48 +0100

    extended hash and validation to work properly with hiera definitions

--- manifests/run_instance.pp
+++ manifests/run_instance.pp
@@ -1,4 +1,8 @@
 # docker::run_instance
-class docker::run_instance($instance) {
-  create_resources(docker::run, $instance)
+class docker::run_instance {
+   $instances = hiera_hash('docker::run_instances', undef)
+   if ($instances) {
+      validate_hash($instances)
+      create_resources(docker::run, $instances)
+   }
 }
