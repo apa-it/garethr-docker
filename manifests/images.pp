47397280267a18950854246baa5994ec1235d30c
Christian Tawfik <christian.tawfik@apa.at>
2015-11-30 18:10:31 +0100

    extended hash and validation to work properly with hiera definitions

--- manifests/images.pp
+++ manifests/images.pp
@@ -1,4 +1,8 @@
 # docker::images
-class docker::images($images) {
-  create_resources(docker::image, $images)
+class docker::images {
+   $images = hiera_hash('docker::images', undef)
+   if ($images) {
+      validate_hash($images)
+      create_resources(docker::image, $images)
+   }
 }
